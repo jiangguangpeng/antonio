e9df164428e712d4ed3252106f6a1102f9e13a66
jiangguangpeng <jiangguangpeng@sina.ch>
2017-09-11 18:28:46 +0800

    commit project

---
+++ PersonRepoDemo/PersonRepoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PersonRepoDemo
+//
+//  Created by antonio on 2017/9/11.
+//  Copyright © 2017年 antonio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PersonRepoDemo/PersonRepoDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PersonRepoDemo
+//
+//  Created by antonio on 2017/9/11.
+//  Copyright © 2017年 antonio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
